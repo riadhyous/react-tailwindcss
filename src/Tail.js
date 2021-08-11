aa32c3e224df708700305f5ad2fa6ab8b887d100
riadhyous <riadhyous@gmail.com>
2021-08-11 16:24:22 +0100

    update navbar

--- src/Tail.js
+++ src/Tail.js
@@ -8,8 +8,8 @@ const navigation = [
   { name: 'Team', href: 'https://www.facebook.com/riadhyous', current: false },
   { name: 'Projects', href: 'https://www.facebook.com/riadhyous', current: false },
   { name: 'Calendar', href: 'https://www.facebook.com/riadhyous', current: true },
-  { name: 'Profil', href: 'https://www.facebook.com/riadhyous', current: false },
-  { name: 'Group', href: 'https://www.facebook.com/riadhyous', current: false },
+  { name: 'Profile', href: 'https://www.facebook.com/riadhyous', current: false },
+  { name: 'Groupe', href: 'https://www.facebook.com/riadhyous', current: false },
 ]
 
 function classNames(...classes) {
@@ -36,16 +36,8 @@ export default function Example() {
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
-                  <img
-                    className="block lg:hidden h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                    alt="Workflow"
-                  />
-                  <img
-                    className="hidden lg:block h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                    alt="Workflow"
-                  />
+                  
+                 
                 </div>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
@@ -76,11 +68,7 @@ export default function Example() {
                   <div>
                     <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                       <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
+                     
                     </Menu.Button>
                   </div>
                   <Transition
